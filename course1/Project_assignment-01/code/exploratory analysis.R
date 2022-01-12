0b36a7b2e691f9e8a3f1bb74eaff07057971d3e8
steven ponce <steven_ponce@yahoo.com>
2022-01-12 08:05:51 -0500

    added course 1 files to directory

---
+++ course1/Project_assignment-01/code/exploratory analysis.R
@@ -0,0 +1,6 @@
+
+# R script file for the exploratory analysis.
+
+# Goal - using the dataset for health care expenditures and consider the question: Is there a relationship between healthcare coverage and healthcare spending in the United States? 
+
+
---
+++ course1/Project_assignment-01/scripts/week-03_assignment-01.R
@@ -0,0 +1,14 @@
+
+# week 03 - assignment 01
+
+
+# If needed, run the following code: 
+# install.packages('ProjectTemplate')
+
+
+# Load library
+library('ProjectTemplate')
+
+# Create new project template
+create.project(project.name = "week-03_assignment-01",
+               template = "minimal")
