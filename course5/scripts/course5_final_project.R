6d0d718f42d481e42bfe84e453823e01159968f2
steven ponce <steven_ponce@yahoo.com>
2022-03-18 18:48:16 -0400

    course 5

---
+++ course5/scripts/course5_final_project.R
@@ -0,0 +1,14 @@
+# Peer-graded Assignment: Modeling Data in the Tidyverse Course Project
+
+# Background
+# In this course, we have learned about modeling data in the Tidyverse in R. This project will give you the opportunity to practice those skills in greater depth. 
+
+# Here, we will continue to use consumer complaints data from the Consumer Complaint Database (CFPB) that was used in the Wranging Data in the Tidyverse Course Project. The CFPB is an independent agency of the United States government that promotes transparency and protects consumers by providing information needed to make decisions when choosing financial institutions including banking institutions, lenders, mortgage services, credit unions, securities firms, foreclosure services, and debt collectors. One of the purposes of the agency is to receive and process complaints and questions about consumer financial products and services. 
+
+# When a complaint is submitted by a consumer, the CFPB has to determine which category the complaint falls in (e.g. "Mortgage", "Student loan", etc). In this project, your goal will be to use the skills you have learned about in this course to build a classification algorithm to classify consumer complaints into one of four categories: "Credit card or prepaid card", "Mortgage", "Student loan", or "Vehicle loan or lease". 
+
+# Data 
+
+# Training data -- complaints from CFPB (data_complaints_train.csv) 
+# Testing data -- complaints from CFPB (data_complaints_test.csv) 
+
--- course5/scripts/course5_predicting_air_pollution.R
+++ course5/scripts/course5_predicting_air_pollution.R
@@ -165,6 +165,322 @@ PM_wflow_fit
 
 ## Assessing the Model Fit ----
 
+# After we fit our model, we can use the broom package to look at the output from the fitted model in an easy/tidy way.
+wflowoutput <- PM_wflow_fit %>% 
+  extract_fit_parsnip() %>% 
+  broom::tidy() 
+
+wflowoutput
+
+# Let’s take a look at the top 10 contributing variables:
+# install.packages('vip')
+library(vip)
+
+PM_wflow_fit %>% 
+  extract_fit_parsnip() %>% 
+  vip(num_features = 10)
+
+
+## Model Performance: Getting Predicted Values ----
+
+#  let’s pull out our predicted outcome values from the models we fit (using different approaches).
+wf_fit <- PM_wflow_fit %>% 
+  extract_fit_parsnip()
+
+wf_fitted_values <- wf_fit$fit$fitted.values
+head(wf_fitted_values)
+
+
+# Alternatively, we can get the fitted values using the augment() function of the broom package using the output from workflows:
+wf_fitted_values <- 
+  broom::augment(wf_fit$fit, data = preproc_train) %>% 
+  select(value, .fitted:.std.resid)
+
+head(wf_fitted_values)
+
+# Finally, we can also use the predict() function. Note that because we use the actual workflow here, we can (and actually need to) use the raw data instead of the preprocessed data.
+values_pred_train <- 
+  predict(PM_wflow_fit, train_pm) %>% 
+  bind_cols(train_pm %>% select(value, fips, county, id)) 
+
+values_pred_train
+
+
+## Visualizing Model Performance ----
+
+# Now, we can compare the predicted outcome values (or fitted values)  to the actual outcome values that we observed:
+
+wf_fitted_values %>% 
+  ggplot(aes(x =  value, y = .fitted)) + 
+  geom_point() + 
+  xlab("actual outcome values") + 
+  ylab("predicted outcome values")
+
+
+## Quantifying Model Performance ----
+
+# One way to calculate these metrics within the tidymodels framework is to use the yardstick package using the metrics() function.
+
+yardstick::metrics(wf_fitted_values, 
+                   truth = value, estimate = .fitted)
+
+
+# Alternatively if you only wanted one metric you could use the mae(), rsq(), or rmse() functions, respectively.
+yardstick::rmse(wf_fitted_values, 
+                truth = value, estimate = .fitted)
+
+
+## Assessing Model Performance on v -folds Using tune ----
+
+# Again, because these are created at random, we need to use the base set.seed() function in order to obtain the same results each time. We will create 10 folds.
+set.seed(1234)
+
+vfold_pm <- rsample::vfold_cv(data = train_pm, v = 10)
+vfold_pm
+
+pull(vfold_pm, splits)
+
+# We can fit the model to our cross validation folds using the fit_resamples() function of the tune package, by specifying our workflow object and the cross validation fold object we just created.
+
+set.seed(122)
+resample_fit <- tune::fit_resamples(PM_wflow, vfold_pm)
+
+# now take a look at various performance metrics based on the fit of our cross validation “resamples”.
+resample_fit
+
+collect_metrics(resample_fit)
+
+
+## Random Forest ---- 
+
+# Now, we are going to predict our outcome variable (air pollution) using a decision tree method called random forest.
+RF_rec <- recipe(train_pm) %>%
+  update_role(everything(), new_role = "predictor")%>%
+  update_role(value, new_role = "outcome")%>%
+  update_role(id, new_role = "id variable") %>%
+  update_role("fips", new_role = "county id") %>%
+  step_novel("state") %>%
+  step_string2factor("state", "county", "city") %>%
+  step_rm("county") %>%
+  step_rm("city") %>% ####
+  step_rm("zcta") %>%
+  step_corr(all_numeric())%>%
+  step_nzv(all_numeric())
+
+# Now that we have our recipe (RF_rec), let’s specify the model with rand_forest() from parsnip.
+library(randomForest)
+
+PMtree_model <- 
+  parsnip::rand_forest(mtry = 10, min_n = 4)
+PMtree_model
+
+# Next, we set the engine and mode:
+RF_PM_model <- 
+  PMtree_model %>%
+  set_engine("randomForest") %>%
+  set_mode("regression")
+
+RF_PM_model 
+
+
+# Then, we put this all together into a workflow
+RF_wflow <- workflows::workflow() %>%
+  workflows::add_recipe(RF_rec) %>%
+  workflows::add_model(RF_PM_model)
+RF_wflow
+
+# Finally, we fit the data to the model:
+RF_wflow_fit <- parsnip::fit(RF_wflow, data = train_pm)
+RF_wflow_fit
+
+# Now, we will look at variable importance:
+RF_wflow_fit %>% 
+  extract_fit_parsnip() %>% 
+  vip(num_features = 10)
+
+# Now let’s take a look at model performance by fitting the data using cross validation:
+set.seed(456)
+resample_RF_fit <- tune::fit_resamples(RF_wflow, vfold_pm)
+collect_metrics(resample_RF_fit)
+
+# OK, so our first model had a mean rmse value of 2.13. It looks like the random forest model had a much lower rmse value of 1.68.
+
+# If we tuned our random forest model based on the number of trees or the value for mtry (which is “The number of predictors that will be randomly sampled at each split when creating the tree models”), we might get a model with even better performance.
+
+# However, our cross validated mean rmse value of 1.68 is quite good because our range of true outcome values is much larger: (3.496, 22.259).
+
+
+## Model Tuning ----
+RF_PM_model <- 
+  parsnip::rand_forest(mtry = 10, min_n = 4) %>%
+  set_engine("randomForest") %>%
+  set_mode("regression")
+
+RF_PM_model
+
+# we can use the tune() function of the tune package like so: mtry = tune(). This indicates that these hyperparameters are to be tuned.
+tune_RF_model <- rand_forest(mtry = tune(), min_n = tune()) %>%
+  set_engine("randomForest") %>%
+  set_mode("regression")
+
+tune_RF_model
+
+# Again we will add this to a workflow
+RF_tune_wflow <- workflows::workflow() %>%
+  workflows::add_recipe(RF_rec) %>%
+  workflows::add_model(tune_RF_model)
+RF_tune_wflow
+
+# You can see how many cores you have access to on your system using the 
+# detectCores() function in the parallel package.
+library(parallel)
+parallel::detectCores()
+
+# The registerDoParallel() function will use the number for cores specified using the cores= argument, or it will assign it automatically to one-half of the number of cores detected by the parallel package.
+doParallel::registerDoParallel(cores=2)
+set.seed(123)
+
+tune_RF_results <- tune_grid(object = RF_tune_wflow, resamples = vfold_pm, grid = 20)
+tune_RF_results
+
+# Now we can use the collect_metrics() function again to take a look at what happened with our cross validation tests. We can see the different values chosen for mtry and min_n and the mean rmse and rsq values across the cross validation samples.
+
+tune_RF_results%>%
+  collect_metrics() %>%
+  head()
+
+# We can now use the show_best() function as it was truly intended, to see what values for min_n and mtry resulted in the best performance.
+show_best(tune_RF_results, metric = "rmse", n =1)
+
+# There we have it… looks like an mtry of 17 and min_n of 4 had the best rmse value 
+
+## Final model performance evaluation -----
+
+# So, first we need to specify these values in a workflow. We can use the select_best() function of the tune package to grab the values that were determined to be best for mtry and min_n.So, first we need to specify these values in a workflow. We can use the select_best() function of the tune package to grab the values that were determined to be best for mtry and min_n.
+
+tuned_RF_values<- select_best(tune_RF_results, "rmse")
+tuned_RF_values
+
+# Now we can finalize the model/workflow that we we used for tuning with these values.
+RF_tuned_wflow <-RF_tune_wflow %>%
+  tune::finalize_workflow(tuned_RF_values)
+
+# the results will show the performance using the testing data.
+overallfit <-tune::last_fit(RF_tuned_wflow, pm_split)
+# or
+overallfit <-RF_wflow %>%
+  tune::last_fit(pm_split)
+
+# To see the performance on the test data we can use the collect_metrics() function like we did before.
+collect_metrics(overallfit)
+
+# ow if you wanted to take a look at the predicted values for the test set (the 292 rows with predictions out of the 876 original monitor values) you can use the collect_predictions() function of the tune package:
+test_predictions <-collect_predictions(overallfit)
+head(test_predictions)
+
+# Now, we can compare the predicted outcome values (or fitted values) to the actual outcome values that we observed:
+test_predictions %>% 
+  ggplot(aes(x =  value, y = .pred)) + 
+  geom_point() + 
+  xlab("actual outcome values") + 
+  ylab("predicted outcome values") +
+  geom_smooth(method = "lm")
+
+
+sessionInfo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
